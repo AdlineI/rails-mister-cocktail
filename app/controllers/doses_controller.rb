4dd3b32bece6861f6d391dc096f631be17b01296
Adline Isiji <adlyne@gmail.com>
2018-11-08 23:50:13 +0100

    Coctails&Dose

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,30 @@
+class DosesController < ApplicationController
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @doses.destroy
+    redirect_to doses_url,
+
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:amount, :ingredient_id)
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+  resources :cocktails, only: [:new, :index, :create, :show]
+  resources :doses, only: [:destroy, :new, :create]
+
+  # resources :reviews, only: [:new, :index, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
